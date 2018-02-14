5828280df581233ccf646e71d997a025a352b288
Jorge García <jorge@aprendible.com>
2018-02-14 11:26:59 -0500

    06 - Instalando Vue Router

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,6 +1,11 @@
 require('./bootstrap');
 
 import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+// rutas
 
 // Vue.component('example', require('./components/Example.vue'));
 
